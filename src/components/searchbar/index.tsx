1a3f99da61de4b3f728961e634e43f39dc093d1e
Ww <nickolaswewe2@gmail.com>
2025-04-21 19:53:08 +0700

    feat: logic for searchbar

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 
 interface SearchBarProps {
@@ -7,20 +8,69 @@ interface SearchBarProps {
     className?: string;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({type,placeholder, className}) => {
+const SearchBar: React.FC<SearchBarProps> = ({ type = "text", placeholder, className })   => {
+  const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const handleSearch = async (e: React.FormEvent) => {
+  e.preventDefault(); 
+  setLoading(true);
+  setError("");
+  setResults([]); 
+
+  try{ 
+    const response = await fetch(" ", { 
+      method: "POST", 
+      headers: { 
+        "Content-Type": "application/json", 
+      },
+      body: JSON.stringify({ query }),
+    }); 
+
+    if(!response.ok) throw new Error("Network response was not ok");
+
+    const data = await response.json();
+    setResults(data.results);
+  } catch (error) {
+  setError("An error occurred while searching. Please try again later.");
+  }finally{ 
+    setLoading(false);
+  }
+  };
+
   return (
     <div className='flex items-center relative w-1/3 mx-auto'>
+      <form onSubmit={handleSearch}>
              <input
-               type="text"
-               placeholder="Search for anything..."
-               className="w-full py-3 pl-4 pr-12 rounded-full border-2 border-black focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-700"
+               type={type}
+               placeholder={placeholder}
+               value={query}
+               onChange={(e) => setQuery(e.target.value)}
+               className={`w-full py-3 pl-4 pr-12 rounded-full border-2 border-black focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-700 ${className}`}
              />
        
              <IoSearch
-               className="absolute  text-gray-500 right-4"
+               className="absolute  text-gray-500 right-4 cursor-pointer"
              />
+      </form> 
+
+      {loading && <p className='text-center'>Loading...</p> }
+      {error && <p className=''>{error}</p>}
+      {results.length > 0 && ( 
+        <div>
+          <h3>Search Result: </h3>
+          <ul> 
+            {results.map((result, index) => ( 
+              <li>
+                
+              </li>
+            ))}
+          </ul>
+        </div> 
+      )}
     </div>
   )
-}
-
+} 
 export default SearchBar
